9d3d348672122c26fb79aee99c7fdced6f61c811
Karthikeya <yelisettikarthik0@gmail.com>
2022-04-16 14:11:57 +0530

    some other commit that idk

---
+++ Task1/a_star.py
@@ -0,0 +1,82 @@
+import cv2
+from cv2 import INTER_AREA
+import numpy as np
+import time
+
+from maze_generation import createMaze
+
+start = time.time()
+
+RED = (0,0,255)
+GREEN = (0,255,0)
+BLUE = (255,0,0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+COLOR2 = (61, 217, 255)
+def find_dist(point, current):
+    return (point[0] - current[0])**2 + (point[1] - current[1])**2
+    #Since we dont need the exact distance, we will keep it as square itself
+
+def inRange(img,point):
+    #checks if point is within the range of the image's dimensions
+    return (point[0]>=0 and point[0]<img.shape[0] and point[1]>=0 and point[1]<img.shape[1])
+
+def dijkstra(img, start, end):
+    n, m = img.shape[:2]
+    parent = np.zeros((n,m,2))
+    visited = []
+    current = start
+    distance = np.full((n,m), np.inf)
+    pixel_distance = np.full((n,m), np.inf)
+    distance[start] = 0
+    pixel_distance[start] = 0
+    while True:
+        if current != end:
+            for (i, j) in [(-1,0), (1,0), (0,-1), (0,1)]:
+                if img.shape[0] < 100:
+                    showImg = cv2.resize(img, (500,500), interpolation=cv2.INTER_AREA)
+                    cv2.imshow("A*", showImg)
+                    cv2.waitKey(1)
+                else:
+                    cv2.imshow("A*", img)
+                newpoint = (current[0]+i, current[1]+j)
+                if inRange(img, newpoint) and not (img[newpoint] == BLACK).all():
+                    pixel_distance[newpoint] = pixel_distance[current] + find_dist(newpoint, current)
+                    if distance[newpoint] > pixel_distance[newpoint]+ find_dist(newpoint, current) + find_dist(newpoint, end):
+                        distance[newpoint] = pixel_distance[newpoint] + find_dist(newpoint, current)+ find_dist(newpoint, end)
+                        img[newpoint] = COLOR2
+                        parent[newpoint] =  current
+                        visited.append(newpoint)
+                min = np.inf
+            for point in visited:
+                if distance[point] < min:
+                    min = distance[point]
+                    current = point
+            visited.remove(current)
+        else:
+            img[end] = BLUE
+            distance[end] = distance[current] +find_dist(end, current)
+            return distance[end], parent
+
+def trackaStar(img, start, end):
+    dist, parent = dijkstra(img, start, end)
+    current = tuple(list(map(int, parent[end])))
+    dist = 1
+    while current != start:
+        dist += 1
+        img[current] = GREEN
+        current = tuple(list(map(int, parent[current])))
+        if img.shape[0] < 100:
+            showImg = cv2.resize(img, (500,500), interpolation=cv2.INTER_AREA)
+            cv2.imshow("A*", showImg)
+            cv2.waitKey(1)
+        else:
+            cv2.imshow("A*", img)
+    print("Distance: " + str(dist) + " pixels")
+
+# start = time.time()
+# maze,startXY,endXY, smaller_maze = createMaze()
+# trackaStar(smaller_maze, startXY, endXY)
+# end = time.time()
+# print("Time: "+str(round(end-start, 4)) + " seconds")
+# cv2.waitKey(0)
---
+++ Task1/dijkstra2.py
@@ -0,0 +1,82 @@
+import cv2
+from cv2 import INTER_AREA
+import numpy as np
+import time
+
+from maze_generation import createMaze
+
+start = time.time()
+
+RED = (0,0,255)
+GREEN = (0,255,0)
+BLUE = (255,0,0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+COLOR2 = (61, 217, 255)
+def find_dist(point, current):
+    return (point[0] - current[0])**2 + (point[1] - current[1])**2
+    #Since we dont need the exact distance, we will keep it as square itself
+
+def inRange(img,point):
+    #checks if point is within the range of the image's dimensions
+    return (point[0]>=0 and point[0]<img.shape[0] and point[1]>=0 and point[1]<img.shape[1])
+
+def dijkstra(img, start, end):
+    n, m = img.shape[:2]
+    parent = np.zeros((n,m,2))
+    visited = []
+    current = start
+    distance = np.full((n,m), np.inf)
+    distance[start] = 0
+    while True:
+        if current != end:
+            for (i, j) in [(-1,0), (1,0), (0,-1), (0,1)]:
+                if img.shape[0] < 100:
+                    showImg = cv2.resize(img, (500,500), interpolation=cv2.INTER_AREA)
+                    cv2.imshow("dijkastra", showImg)
+                    cv2.waitKey(1)
+                else:
+                    cv2.imshow("dijkastra", img)
+                newpoint = (current[0]+i, current[1]+j)
+                if inRange(img, newpoint) and not (img[newpoint] == BLACK).all():
+                    if distance[newpoint] > distance[current] + find_dist(newpoint, current):
+                        distance[newpoint] = distance[current] + find_dist(newpoint, current)
+                        img[newpoint] = COLOR2
+                        parent[newpoint] =  current
+                        visited.append(newpoint)
+                min = np.inf
+            for point in visited:
+                if distance[point] < min:
+                    min = distance[point]
+                    current = point
+            visited.remove(current)
+        else:
+            img[end] = BLUE
+            distance[end] = distance[current] +find_dist(end, current)
+            return distance[end], parent
+
+def trackDijkastra(inimg, start, end):
+    img = inimg
+    dist, parent = dijkstra(img, start, end)
+    current = tuple(list(map(int, parent[end])))
+    while current != start:
+        img[current] = GREEN
+        current = tuple(list(map(int, parent[current])))
+        if img.shape[0] < 100:
+            showImg = cv2.resize(img, (500,500), interpolation=cv2.INTER_AREA)
+            cv2.imshow("dijkastra", showImg)
+            cv2.waitKey(1)
+        else:
+            cv2.imshow("dijkastra", img)
+    print("Distance: " + str(int(dist)) + " pixels")
+
+# start = time.time()
+# maze,startXY,endXY, smaller_maze = createMaze()
+# trackDijkastra(smaller_maze, startXY, endXY)
+# end = time.time()
+# print("Time: "+str(round(end-start, 4)) + " seconds")
+# cv2.waitKey(0)
+# cv2.imshow('Maze',maze)
+# cv2.imshow('Dijkstra Path',path_dj_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
