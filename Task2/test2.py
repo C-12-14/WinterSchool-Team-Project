2fcf28f5a9297d3a511ec61be7244030dad02956
C-12 <xe54c13@gmail.com>
2022-04-17 18:04:37 +0530

    works now 'test2.py'

--- Task2/test2.py
+++ Task2/test2.py
@@ -35,35 +35,49 @@ classes = ('20','30','50','60','70','80',
            'Ahead only',      
            'Go straight or right',      
            'Go straight or left',      
-           'Keep right',     
+           'Red Traffic Light',     
            'Keep left',      
-           'Roundabout mandatory',     
+           'Green Traffic Light',     
            'End of no passing',      
            'End no passing veh > 3.5 tons')
-
-
 model = load_model('traffic_classifier.h5')
-
 cap = cv.VideoCapture('Task2/video.mp4')
+sign = 0
+diameter = 0.1524        # diameter = 6 inches or 15.24 cm
 
 while True:
-    success, frame = cap.read()
+    with open("Task2/output.txt", "a") as myfile:
+        success, frame = cap.read()
+        if not success:
+            break
+        image = Image.fromarray(frame)
+        image = image.resize((30, 30))
+        image = np.expand_dims(image, axis=0)
+        image = np.array(image)
+        pred = model.predict([image]).argmax()
+        tempsign = classes[pred]
+        if tempsign != sign:
+            sign = tempsign
+            if sign in ["Stop", "Red Traffic Light"]:
+                myfile.write("Stop Motor\n")
+                print(sign)
+            elif sign == "Green Traffic Light":
+                myfile.write("Start Motor\n")
+                print(sign)
+            elif sign in ['20','30','50','60','70','80','100','120'] :
+                rpm = (50*int(sign))/(3*np.pi*diameter)             # calculating rpm from speed(km/hr)
+                myfile.write("%.2f"%rpm)
+                myfile.write("\n")
+                print(sign + " km/hr")
+            else:
+                myfile.write(sign + "\n")
+                print(sign)
 
-    image = Image.fromarray(frame)
-    image = image.resize((30, 30))
-    image = np.expand_dims(image, axis=0)
-    image = np.array(image)
-    pred = model.predict([image]).argmax()
-    sign = classes[pred]
-    print(sign)
 
-    if not success:
-        break
+        cv.imshow('Output', frame)
 
-    cv.imshow('Output', frame)
-
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv.destroyAllWindows()
